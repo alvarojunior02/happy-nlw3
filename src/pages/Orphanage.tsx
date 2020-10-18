a60fa1be4c53b1aee83959dbfa6d2fe05640dba9
alvarojunior02 <alvaroamcjunior8@gmail.com>
2020-10-18 00:35:24 -0400

    Add component 'Sidebar'

--- src/pages/Orphanage.tsx
+++ src/pages/Orphanage.tsx
@@ -2,35 +2,16 @@ import React from "react";
 import { FaWhatsapp } from "react-icons/fa";
 import { FiClock, FiInfo, FiArrowLeft } from "react-icons/fi";
 import { Map, Marker, TileLayer } from "react-leaflet";
-import { useHistory } from 'react-router-dom';
-import L from 'leaflet';
-
-import mapMarkerImg from '../images/map-marker.svg';
 
 import '../styles/pages/orphanage.css';
-
-const happyMapIcon = L.icon({
-  iconUrl: mapMarkerImg,
-
-  iconSize: [58, 68],
-  iconAnchor: [29, 68],
-  popupAnchor: [0, -60]
-})
+import Sidebar from "../components/Sidebar";
+import mapIcon from "../utils/mapIcon";
 
 export default function Orphanage() {
-  const { goBack } = useHistory();
-
   return (
     <div id="page-orphanage">
-      <aside>
-        <img src={mapMarkerImg} alt="Happy" />
-
-        <footer>
-          <button type="button" onClick={goBack}>
-            <FiArrowLeft size={24} color="#FFF" />
-          </button>
-        </footer>
-      </aside>
+      
+      <Sidebar />
 
       <main>
         <div className="orphanage-details">
@@ -63,7 +44,7 @@ export default function Orphanage() {
 
             <div className="map-container">
               <Map 
-                center={[-27.2092052,-49.6401092]} 
+                center={[-15.2346725,-59.3297667]} 
                 zoom={16} 
                 style={{ width: '100%', height: 280 }}
                 dragging={false}
@@ -73,9 +54,9 @@ export default function Orphanage() {
                 doubleClickZoom={false}
               >
                 <TileLayer 
-                  url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+                  url={`https://a.tile.openstreetmap.org/{z}/{x}/{y}.png`}
                 />
-                <Marker interactive={false} icon={happyMapIcon} position={[-27.2092052,-49.6401092]} />
+                <Marker interactive={false} icon={mapIcon} position={[-15.2346725,-59.3297667]} />
               </Map>
 
               <footer>
